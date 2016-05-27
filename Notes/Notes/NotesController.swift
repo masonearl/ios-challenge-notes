c0c5ee19cf2579e1fac6b76f9c0c8b7d04d5fab7
buildmase <mason.earl@icloud.com>
2016-05-27 10:10:53 -0600

    NoteController: Check

--- Notes/Notes/NotesController.swift
+++ Notes/Notes/NotesController.swift
@@ -7,3 +7,27 @@
 //
 
 import Foundation
+
+class NotesController {
+    
+    static let sharedInstance = NotesController()
+    var notes: [Note]
+    private let notesKey = "notes"
+    
+    init(){
+        self.notes = [] }
+    
+    func addNote(note: Note) {
+        notes.append(note) }
+    
+    func removeNote(note: Note) {
+        if let index = notes.indexOf(note) {
+            notes.removeAtIndex(index) }}
+    
+    func saveToPersistentStorage() {
+        NSUserDefaults.standardUserDefaults().setObject(notes.map{$0.dictionaryCopy}, forKey: notesKey )}
+    
+    func loadFromPersistentStorage() {
+        guard let notesDictionaryArray = NSUserDefaults.standardUserDefaults().objectForKey(notesKey) as? [[String: AnyObject]] else { return }
+        notes = notesDictionaryArray.flatMap{Note(dictionary: $0) }}}
+
--- Notes/Notes/NotesDetailViewController.swift
+++ Notes/Notes/NotesDetailViewController.swift
@@ -19,16 +19,26 @@ class NotesDetailViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
     @IBAction func clearButtonTapped(sender: AnyObject) {
+        self.titleTextField.text = ""
+        self.textView.text = ""
         
     }
 
     @IBAction func saveButtonTapped(sender: AnyObject) {
+//        if let title = title {
+//            guard let title = titleTextField.text, text = textView.text
+        }
         
+    
     }
-}
